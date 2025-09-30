1f256be13d662564d02639d48ed53875eeb96986
BPplays <suzuko.oss@proton.me>
2025-09-30 10:02:01 -0700

    go

--- .src/doh_lookup/main.go
+++ .src/doh_lookup/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 
+
+	"github.com/miekg/dns"
 	"gopkg.in/yaml.v3"
 )
 
@@ -17,6 +23,7 @@ var typeMap map[string]string = map[string]string{
 
 
 type Config struct {
+	Resolvers []string `yaml:"resolvers"`
 	Lists []List `yaml:"lists"`
 }
 
@@ -57,10 +64,76 @@ func readConfig(file string) (Config) {
 			continue
 		}
 
-		break
+		return cfg
 	}
 
+	log.Fatalln("can't read config")
 	return cfg
+}
+
+func queryWithResolvers(domain string, qtypes []uint16, resolvers []string, timeout time.Duration, tries int) ([]string, error) {
+	client := &dns.Client{Timeout: timeout}
+	seen := map[string]struct{}{}
+	results := []string{}
+
+	for _, qtype := range qtypes {
+		for _, resolver := range resolvers {
+			var lastErr error
+			for attempt := 0; attempt < tries; attempt++ {
+				msg := new(dns.Msg)
+				msg.SetQuestion(dns.Fqdn(domain), qtype)
+				in, _, err := client.Exchange(msg, resolver)
+				if err != nil {
+					lastErr = err
+					// retry same resolver up to tries
+					continue
+				}
+				if in == nil || in.Rcode != dns.RcodeSuccess {
+					lastErr = fmt.Errorf("bad rcode: %v", in)
+					continue
+				}
+				for _, ans := range in.Answer {
+					switch v := ans.(type) {
+					case *dns.AAAA:
+						if _, ok := seen[v.AAAA.String()]; !ok {
+							seen[v.AAAA.String()] = struct{}{}
+							results = append(results, v.AAAA.String())
+						}
+					case *dns.A:
+						if _, ok := seen[v.A.String()]; !ok {
+							seen[v.A.String()] = struct{}{}
+							results = append(results, v.A.String())
+						}
+					}
+				}
+				// break out of retry loop for this resolver (we got a response)
+				lastErr = nil
+				break
+			}
+			// if this resolver failed completely, try next resolver
+			if lastErr != nil {
+				// you can log lastErr if you want
+				continue
+			}
+			// if we already have answers for this qtype, optionally stop trying other resolvers
+			// here we continue so that we collect A and AAAA from all resolvers if they exist
+		}
+	}
+
+	if len(results) == 0 {
+		return nil, fmt.Errorf("no answers from any resolver")
+	}
+	return results, nil
+}
+
+
+func preCheck(cfg Config) {
+	checkDomains := []string{"example.com", "google.com"}
+	timeout := 5 * time.Second
+	tries := 5
+
+	queryWithResolvers()
+
 }
 
 func main() {
