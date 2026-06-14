82323cdb58f0c2b55365610cff9bde3a82b543da
BPplays <suzuko.oss@proton.me>
2026-06-14 05:33:06 -0700

    better logging

--- .src/doh_lookup/main.go
+++ .src/doh_lookup/main.go
@@ -1235,7 +1235,10 @@ func main() {
 	}
 	if len(customNat64) > 0 {
 		Nat64Prefixs = append(Nat64Prefixs, customNat64...)
-		fmt.Printf("using custom NAT64 prefixes: %v\n", Nat64Prefixs)
+	}
+
+	if len(Nat64Prefixs) > 0 {
+		log.Printf("using NAT64 prefixes: %v\n", Nat64Prefixs)
 	}
 
 
