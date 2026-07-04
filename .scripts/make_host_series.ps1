68510218c04a26261e6505375d138d4684991136
BPplays <suzuko.oss@proton.me>
2026-07-03 20:04:02 -0700

    test2

--- .scripts/make_host_series.ps1
+++ .scripts/make_host_series.ps1
@@ -1,83 +1,66 @@
-function New-HostSeries {
-    param(
-        [string[]]$Hosts,
-        [int]$Start = 1,
-        [int]$End = 64,
-        [ValidateSet('0','00','000','alpha','aa')]
-        [string[]]$Formats = @('0','00','000','alpha')
-    )
+# .\make_host_series.ps1 -Hosts 'doh-$.spectrum.com','dns-$.spectrum.com' -Start 1 -End 64 -Formats 0,00,000,alpha
+param(
+    [string[]]$Hosts,
+    [int]$Start = 1,
+    [int]$End = 64,
+    [ValidateSet('0','00','000','alpha')]
+    [string[]]$Formats = @('0','00','000','alpha')
+)
 
-    function Get-AlphaRange {
-        param([int]$Count)
+function Get-AlphaRange {
+    param([int]$Count)
 
-        $result = @()
+    $result = @()
 
-        for ($i = 0; $i -lt $Count; $i++) {
-            $n = $i
-            $s = ""
+    for ($i = 0; $i -lt $Count; $i++) {
+        $n = $i
+        $s = ""
 
-            do {
-				$s = [char][int](97 + ($n % 26)) + $s
-                $n = [math]::Floor($n / 26) - 1
-            } while ($n -ge 0)
+        do {
+			$s = [char][int](97 + ($n % 26)) + $s
+            $n = [math]::Floor($n / 26) - 1
+        } while ($n -ge 0)
 
-            $result += $s
-        }
-
-        return $result
+        $result += $s
     }
 
-    $numbers = $Start..$End
-    $alpha   = Get-AlphaRange ($End - $Start + 1)
+    return $result
+}
 
-    $out = foreach ($hostTemplate in $Hosts) {
+$numbers = $Start..$End
+$alpha   = Get-AlphaRange ($End - $Start + 1)
 
-        foreach ($fmt in $Formats) {
+$result = foreach ($hostTemplate in $Hosts) {
 
-            switch ($fmt) {
+    foreach ($fmt in $Formats) {
 
-                '0' {
-                    foreach ($n in $numbers) {
-                        $hostTemplate.Replace('$', $n)
-                    }
+        switch ($fmt) {
+
+            '0' {
+                foreach ($n in $numbers) {
+                    $hostTemplate.Replace('$', $n)
                 }
+            }
 
-                '00' {
-                    foreach ($n in $numbers) {
-                        $hostTemplate.Replace('$', ('{0:00}' -f $n))
-                    }
+            '00' {
+                foreach ($n in $numbers) {
+                    $hostTemplate.Replace('$', ('{0:00}' -f $n))
                 }
+            }
 
-                '000' {
-                    foreach ($n in $numbers) {
-                        $hostTemplate.Replace('$', ('{0:000}' -f $n))
-                    }
+            '000' {
+                foreach ($n in $numbers) {
+                    $hostTemplate.Replace('$', ('{0:000}' -f $n))
                 }
+            }
 
-                'alpha' {
-                    foreach ($a in $alpha) {
-                        $hostTemplate.Replace('$', $a)
-                    }
-                }
-
-                'aa' {
-                    foreach ($a in $alpha) {
-                        $hostTemplate.Replace('$', $a)
-                    }
+            'alpha' {
+                foreach ($a in $alpha) {
+                    $hostTemplate.Replace('$', $a)
                 }
             }
         }
     }
-
-    $out | Select-Object -Unique
 }
 
-
-New-HostSeries `
-  -Hosts @(
-    'doh-$.spectrum.com',
-    'dns-$.spectrum.com'
-  ) `
-  -Start 1 -End 64 `
-  -Formats @('0','00','000','alpha')
-
+$result | Select-Object -Unique
