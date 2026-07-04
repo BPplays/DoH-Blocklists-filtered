244ee092fbc260d0c85b808afc9a397e85fd79c2
BPplays <suzuko.oss@proton.me>
2026-07-03 20:10:19 -0700

    started script for generating good lists

--- .scripts/make_host_series.ps1
+++ .scripts/make_host_series.ps1
@@ -3,32 +3,54 @@ param(
     [string[]]$Hosts,
     [int]$Start = 1,
     [int]$End = 64,
+    [string]$AlphaStart = 'a',
+    [string]$AlphaEnd = 'az',
     [ValidateSet('0','00','000','alpha')]
     [string[]]$Formats = @('0','00','000','alpha')
 )
 
+function Convert-StringToIndex {
+    param([string]$Name)
+    $sum = 0
+    $mult = 1
+    for ($i = $Name.Length - 1; $i -ge 0; $i--) {
+        $sum += (([int]$Name[$i] - [int][char]'a' + 1) * $mult)
+        $mult *= 26
+    }
+    return $sum - 1
+}
+
+function Convert-IndexToString {
+    param([int]$Idx)
+    if ($Idx -lt 26) {
+        return [char](97 + $Idx)
+    }
+    $idx += 1
+    $result = ""
+    while ($idx -gt 0) {
+        $rem = (($idx - 1) % 26)
+        $result = [char](97 + $rem) + $result
+        $idx = [math]::Floor(($idx - 1) / 26)
+    }
+    return $result
+}
+
 function Get-AlphaRange {
-    param([int]$Count)
+    param([string]$Start, [string]$End)
+
+    $startIdx = Convert-StringToIndex $Start
+    $endIdx   = Convert-StringToIndex $End
+    $count    = $endIdx - $startIdx + 1
 
     $result = @()
-
-    for ($i = 0; $i -lt $Count; $i++) {
-        $n = $i
-        $s = ""
-
-        do {
-			$s = [char][int](97 + ($n % 26)) + $s
-            $n = [math]::Floor($n / 26) - 1
-        } while ($n -ge 0)
-
-        $result += $s
+    for ($i = 0; $i -lt $count; $i++) {
+        $result += (Convert-IndexToString ($startIdx + $i))
     }
-
     return $result
 }
 
 $numbers = $Start..$End
-$alpha   = Get-AlphaRange ($End - $Start + 1)
+$alpha   = Get-AlphaRange $AlphaStart $AlphaEnd
 
 $result = foreach ($hostTemplate in $Hosts) {
 
